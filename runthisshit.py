496e1ffda55131d253b6821ae7852fb236ad518f
Ben Graham <graham.ben61@gmail.com>
2018-04-23 17:29:22 -0500

    python script to normalize genres

---
+++ runthisshit.py
@@ -0,0 +1,10 @@
+# Normalize the genres from the movie table
+
+import csv 
+input = csv.reader(open('movie_data/movies.csv'), delimiter=",") 
+output = csv.writer(open('movie_data/genres-normalized.csv', 'w'), delimiter=",") 
+for row in input:
+	print row
+	if row:
+		for cell in row[2].split("|"):
+			output.writerow([row[0], cell])
